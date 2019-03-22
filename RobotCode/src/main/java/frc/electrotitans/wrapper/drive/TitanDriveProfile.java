c9399af20b118b686006509c1cc723e1be801fc2
victoryforphil <victoryforphil@gmail.com>
2019-03-22 05:40:46 -0700

    Removed Wrapper

--- RobotCode/src/main/java/frc/electrotitans/wrapper/drive/TitanDriveProfile.java
+++
@@ -1,9 +0,0 @@
-package frc.electrotitans.wrapper.drive;
-
-public class TitanDriveProfile{
-    private double weight;
-    private double num_motors = 0;
-    private double motor_rpm = 0;
-
-    
-}
--- RobotCode/src/main/java/frc/electrotitans/wrapper/motorcontrollers/TitanSpark.java
+++
@@ -1,2 +0,0 @@
-package frc.electrotitans.wrapper.motorcontrollers;
-
